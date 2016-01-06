bf52999641da9a6c921ffb8435acd05eb944553c
Yi Li <yl2326@cornell.edu>
2016-01-06 17:23:32 -0500

    asdf

--- RunJumpFight/RunJumpFight/js/game.js
+++ RunJumpFight/RunJumpFight/js/game.js
@@ -40,7 +40,7 @@ var game = {
     // Run on game resources loaded.
     "loaded": function () {
 
-        me.debug.renderHitBox = true;
+        //me.debug.renderHitBox = true;
         me.state.set(me.state.MENU, new game.TitleScreen());
         me.state.set(me.state.PLAY, new game.PlayScreen());
 
